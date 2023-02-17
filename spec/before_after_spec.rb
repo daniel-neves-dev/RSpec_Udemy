7f64fe5a760bf7a296f64c8bc66ae103b25f21c2
Daniel <daniel.oliveira.au@hotmail.com>
2023-02-17 19:09:21 -0300

    feat: before_after file

---
+++ spec/before_after_spec.rb
@@ -0,0 +1,10 @@
+RSpec.describe 'befor and after methods' do
+    
+    it 'times math' do
+        expect(4*2).to eq(8)
+    end
+
+    it 'minus math' do
+        expect(5-2).to eq(3)
+    end
+end
