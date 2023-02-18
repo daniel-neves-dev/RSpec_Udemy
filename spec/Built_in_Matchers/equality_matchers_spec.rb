3b6c05035d3390c4e043c39a92dfaad1438548e2
Daniel <daniel.oliveira.au@hotmail.com>
2023-02-18 14:37:42 -0300

    feat: equal and be matchers

--- spec/Built_in_Matchers/equality_matchers_spec.rb
+++ spec/Built_in_Matchers/equality_matchers_spec.rb
@@ -7,6 +7,10 @@ RSpec.describe 'equality matchers' do
     it 'test value and ignores type' do
       expect(a).to eq(3)
       expect(b).to eq(3.0)
+      expect(a).to eq(b)
+
+      expect(a).to eq(3.0)
+      expect(b).to eq(3)
     end
   end
 
@@ -14,6 +18,22 @@ RSpec.describe 'equality matchers' do
     it 'test value and the type' do
       expect(a).to eql(3.0)
       expect(b).to eql(3)
+
+      expect(a).not_to eql(3)
+      expect(b).not_to eql(3.0)
+      expect(a).not_to eql(b)
+    end
+  end
+
+  context 'equal and be matchers' do
+    let(:c){[1,2,3]} #c and d are similar but not identical
+    let(:d){[1,2,3]}
+    let(:e){c} # c and e are identical
+
+    it 'cares about identity' do
+      expect(c).to equal(e)
+      expect(c).to be(e)
+      expect(d).not_to equal(c)
     end
   end
 end
