513e3d416acff6a33c3c08032f699397c550b155
Daniel <daniel.oliveira.au@hotmail.com>
2023-02-18 11:44:04 -0300

    refactor: shared_contex-spec

--- spec/Subjects_Shareds/shared_context_spec.rb
+++ spec/Subjects_Shareds/shared_context_spec.rb
@@ -10,3 +10,18 @@ RSpec.shared_context 'common' do
 
   let(:some_variable) {[1,2,3]}
 end
+
+RSpec.describe 'first example group' do
+  include_context 'common' 
+
+  it 'can use instance variables' do
+    expect(@food.length).to eq(0)
+    @food << 'Sushi'
+    expect(@food.length).to eq(1)
+  end
+
+  it 'can share methods' do
+    expect(some_method).to eq(5)
+  end
+  
+end
