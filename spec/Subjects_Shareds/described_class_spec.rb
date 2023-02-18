269f7d1f56994ef2136ef19c94e4e1483d029c5e
Daniel <daniel.oliveira.au@hotmail.com>
2023-02-18 10:28:48 -0300

    refactored: described_class_spec

--- spec/Subjects_Shareds/described_class_spec.rb
+++ spec/Subjects_Shareds/described_class_spec.rb
@@ -8,11 +8,17 @@ end
 
 RSpec.describe King do
   subject {described_class.new('Daniel')} #If the name of the class change, only chage it in RSpec.describe 'class Name'
-  let(:boris) {described_class.new('Boris')}
+  let(:variable) {described_class.new('Boris')}
 
   context 'subject with no variable' do
     it 'respresents a great person' do
       expect(subject.name).to eq('Daniel')
+    end
+  end
+
+  context 'variable subject' do
+    it'also respresents a great person' do
+      expect(variable.name).to eq('Boris')
     end
   end
 
