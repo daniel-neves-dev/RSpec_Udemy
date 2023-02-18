b4ecc1cd5eddbd249b7d4487bfccabf20b871c31
Daniel <daniel.oliveira.au@hotmail.com>
2023-02-18 15:34:09 -0300

    fix : bug fix predicate_methods to be_even

--- spec/Built_in_Matchers/predicate_methods_spec.rb
+++ spec/Built_in_Matchers/predicate_methods_spec.rb
@@ -6,5 +6,11 @@ RSpec.describe 'predicate methods and matchers' do
 
   it 'tested with predicate matchers' do
     expect(16/2).to be_even
+    expect(15).to be_odd
+    expect([]).to be_empty
+  end
+
+  context 20 do
+    it{is_expected.to be_even}
   end
 end
