180cf9fde53c0bbf8e99379a4f9c885caebaf27a
Daniel <daniel.oliveira.au@hotmail.com>
2023-02-18 10:58:56 -0300

    refactored: shared_examples_spec

--- spec/Subjects_Shareds/shared_examples_spec.rb
+++ spec/Subjects_Shareds/shared_examples_spec.rb
@@ -1,15 +1,25 @@
-Rspec.describe Array do
+RSpec.shared_examples 'a Ruby object with three elements' do
+  it 'return the numbers of itens' do
+    expect(subject.length).to eq(3)
+  end
+end
+
+RSpec.describe Array do
   subject {[1,2,3]}
+  include_examples 'a Ruby object with three elements'
 end
 
 RSpec.describe String do
   subject {'abc'}
+  include_examples 'a Ruby object with three elements'
 end
 
 RSpec.describe Hash do
   subject { {a: 1, b: 2, c: 3} }
+  include_examples 'a Ruby object with three elements'
 end
 
+#Creating Class
 class MyClass 
   def length
     3
@@ -18,4 +28,5 @@ end
 
 RSpec.describe MyClass do
   subject {described_class.new}
+  include_examples 'a Ruby object with three elements'
 end
