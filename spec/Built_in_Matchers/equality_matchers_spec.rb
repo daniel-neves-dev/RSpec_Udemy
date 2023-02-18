a5aea9b9a043666f2d0b1b73ab776519899d5f46
Daniel <daniel.oliveira.au@hotmail.com>
2023-02-18 14:18:43 -0300

    refactore: not_to_method_spec eql code

--- spec/Built_in_Matchers/equality_matchers_spec.rb
+++ spec/Built_in_Matchers/equality_matchers_spec.rb
@@ -2,13 +2,17 @@ RSpec.describe 'equality matchers' do
   
   let(:a){3.0}
   let(:b){3}
-  let(:c){'Hello'}
 
   context 'eq matcher' do
     it 'test value and ignores type' do
       expect(a).to eq(3)
       expect(b).to eq(3.0)
-      expect(c).to eq('hello')
+    end
+  end
+
+  context 'eql matcher' do
+    it 'test value and the type' do
+      expect(a).to eql(3)
     end
   end
 end
