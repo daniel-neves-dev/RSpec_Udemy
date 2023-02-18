93a3a2e18705ec58d60cd5d3df12f317b4c356e2
Daniel <daniel.oliveira.au@hotmail.com>
2023-02-18 15:30:47 -0300

    refactor: predicate_methods

--- spec/Built_in_Matchers/predicate_methods_spec.rb
+++ spec/Built_in_Matchers/predicate_methods_spec.rb
@@ -1,4 +1,10 @@
 RSpec.describe 'predicate methods and matchers' do
-  result = 16/2
-  expect(result.even?).to be_truth
+  it 'tested with Ruby methods' do
+    result = 16/2
+    expect(result.even?).to be(true)
+  end
+
+  it 'tested with predicate matchers' do
+    expect(16/2).to be_even
+  end
 end
